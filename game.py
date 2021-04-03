ae4639a665277a76fbf9ae21ec7120a5b07a529b
Costa <clambro.git@gmail.com>
2021-04-03 14:14:26 -0700

    Updated game documentation

--- game.py
+++ game.py
@@ -1,41 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun  4 21:47:42 2017
-
-@author: Costa
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 import win32api as wapi
 
 
-class Game(object):
+class Game:
     """A single game of 2048.
 
-    A game consists of a 4x4 board of tiles with integer values representing
-    the log2 of the tile's value.  The tiles can be shifted up, down, left, or
-    right, and when two tiles of the same value touch, they merge and their
-    value is doubled.  If the board is full and there are no legal moves, the
-    game ends.
+    A game consists of a 4x4 board of tiles with integer values representing the log2 of the tile's value.  The tiles
+    can be shifted up, down, left, or right, and when two tiles of the same value touch, they merge and their value is
+    doubled.  If the board is full and there are no legal moves, the game ends.
 
-    Attributes:
-        board: A 4x4 numpy array of the log2 tile value at each board position.
-        score: An integer. The current score.
-        highest_tile: An integer. The highest-value tile on the board.
-        moves: An integer. The number of moves made in the current game.
-        game_over: A boolean describing whether or not the game is over.
-        last_move_illegal: A boolean to flag illegal moves.
+    Attributes
+    ----------
+    board : ndarray
+        A 4x4 integer array of the log2 tile value at each board position.
+    score : int
+        The current score.
+    highest_tile : int
+        The highest-value tile on the board.
+    moves : int
+        The number of moves made in the current game.
+    game_over : bool
+        Whether or not the game is over.
+    last_move_illegal : bool
+        Whether or not the last attempted move was illegal.
     """
-
     def __init__(self):
-        """Inits Game class.
-
-        Places two tiles randomly on the board and sets highest_tile
-        accordingly. Score starts at 0, game_over and last_move_illegal are
-        False, and clears the key-state.
-        """
+        """Sets up the board and clears the key-state."""
         self.board = np.zeros((4, 4), dtype=np.int)
         self.add_tile()
         self.add_tile()
@@ -49,27 +41,22 @@ class Game(object):
             wapi.GetAsyncKeyState(key)
 
     def add_tile(self):
-        """Adds a 2 or 4 tile randomly to the current game board.
-
-        Precondition: Board must not already be full.
-        """
-        # 10% chance the new tile is a 4, else it's a 2.
+        """Adds a 2 or 4 tile randomly to the current game board."""
+        # In 2048, there is a 10% chance of a 4 being added instead of a 2.
         if np.random.random() > 0.9:
             val = 2
         else:
             val = 1
-        # Choose a random empty space to add the tile to
         board = self.board.reshape(16)
         valid_pos = [i for i in range(16) if not board[i]]
         pos = np.random.choice(valid_pos)
         board[pos] = val
         self.board = board.reshape((4, 4))
-        # Game over contition: Board is full and no legal moves.
         if self.board.all() and self.no_moves():
             self.game_over = True
 
     def no_moves(self):
-        """Return True if there are no legal moves, else false."""
+        """Return True if there are no legal moves, else False."""
         orig_board = np.copy(self.board)
         orig_score = np.copy(self.score).tolist()
         # Moves in order [left, up, right, down]
@@ -83,11 +70,14 @@ class Game(object):
                 self.last_move_illegal = False
         return True
 
-    def read_key(self):
-        """Read key from keyboard.
+    @staticmethod
+    def read_key():
+        """Reads a key from keyboard inputs.
 
-        Returns:
-            key: An integer representing the ASCII value of the key pressed.
+        Returns
+        -------
+        key : int
+            The ordinal value of the key pressed.
         """
         while True:
             time.sleep(0.1)
@@ -97,7 +87,7 @@ class Game(object):
                     return key
 
     def slide_left(self):
-        """Slide tiles left one column at a time, but don't merge them."""
+        """Slides tiles left one column at a time, but doesn't merge them."""
         for row in range(4):
             new_row = [i for i in self.board[row, :] if i != 0]
             new_row = new_row + [0 for i in range(4-len(new_row))]
@@ -113,13 +103,14 @@ class Game(object):
                     self.score += 2**self.board[i, j]
 
     def move(self, key):
-        """Execute move in direction of key.
+        """Execute a move in the direction of key.
 
-        Rotate the board so that key points leftwards, move left, then rotate
-        back to the original position.
+        Rotate the board so that key points leftwards, move left, then rotate back to the original position.
 
-        Args:
-            key: An integer representing the ASCII value of the key pressed.
+        Parameters
+        ----------
+        key : int
+            The ordinal value of the key pressed.
         """
         prev_board = np.copy(self.board)
         if key == 37:
@@ -131,7 +122,7 @@ class Game(object):
         else:
             rot = -1
         self.board = np.rot90(self.board, rot)
-        # Slide, merge, slide pattern reflects official 2048 rules
+        # Slide, merge, slide pattern reflects the official 2048 rules
         self.slide_left()
         self.merge_left()
         self.slide_left()
@@ -143,8 +134,10 @@ class Game(object):
     def process_key(self, key):
         """Quit, or move according to key. Ignore illegal moves.
 
-        Args:
-            key: An integer representing the ASCII value of the key pressed.
+        Parameters
+        ----------
+        key : int
+            The ordinal value of the key pressed.
         """
         # ord('Q') = 81.  Quit game.
         if key == 81:
@@ -157,24 +150,27 @@ class Game(object):
         self.highest_tile = np.max(2**self.board)
 
     def display_game(self, axes=None):
-        """Display board using pyplot.
+        """Display the board as a matplotlib figure.
 
-        Args:
-            axes: Pyplot axes. Use to plot over previous figure.
+        Parameters
+        ----------
+        axes : Optional[Axes]
+            Used to plot over previous figure, otherwise a new Axes object is generated.
 
-        Returns:
-            axes: Pyplot axes for the current figure.
+        Returns
+        -------
+        axes : Axes
+            The current figure's Axes.
         """
-        if not axes:
+        if axes is None:
             fig, axes = plt.subplots()
-            show_flag = True  # Whether or not we need to run plt.show()
-            plt.pause(0.5)  # Gives you a bit of time to adjust the plot
+            show_flag = True
+            plt.pause(1)  # Gives you a bit of time to adjust the plot
         else:
             show_flag = False
             axes.clear()
         axes.set_title('Score = ' + str(self.score))
         plt.imshow(self.board, cmap='summer')
-        # Print numbers on tile if log2(tile) != 1
         for (j, i), label in np.ndenumerate(2**self.board):
             if label != 1:
                 axes.text(i, j, label, ha='center', va='center')
